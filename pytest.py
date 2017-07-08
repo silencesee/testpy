b3c31db3d53b4fcc5aa8f0c7e87a6c76196e5585
sinlencesee <858713765@qq.com>
2017-07-08 18:47:26 +0800

    Default

--- pytest.py
+++ pytest.py
@@ -1,17 +1,18 @@
+# encoding: UTF-8
+import sys
 import hashlib
 
 class Passw(object):
-
-    def __init__(self,source,mode = 'md5',*salt):
+    def __init__(self, source, mode='md5', *salt):
         self.source = source
         self.mode = mode
         self.salt = salt
         # self.result = self.cal()
 
     def __str__(self):
-        return 'md5 object : %s' %self.source
+        return 'md5 object : %s' % self.source
 
-    def cal(self):#调用计算函数
+    def cal(self):  # 调用计算函数
         if self.mode == '1':
             str = hashlib.md5()
         elif self.mode == '2':
@@ -21,15 +22,21 @@ class Passw(object):
 
             # print(ss)
         str.update(self.source.encode('utf-8'))
-            # print(str.hexdigest())
+        # print(str.hexdigest())
         return str.hexdigest()
 
     __repr__ = __str__
 
-if __name__=='__main__':
-    str = input('请输入密码')
-    choose = input('请选择加密模式 1.MD5 2.SHA1')
-    s=Passw(str,choose)
+
+if __name__ == '__main__':
+    if '2.7' in sys.version:
+        str = raw_input('请输入密码: ')
+        choose = raw_input('请选择加密模式 1.MD5 2.SHA1 :')
+    else:
+        str = input('请输入密码: ')
+        choose = input('请选择加密模式 1.MD5 2.SHA1 :')
+
+    s = Passw(str, choose)
     md5str1 = s.cal()
-    print('加密结果是：'+md5str1)
+    print('加密结果是：' + md5str1)
     exit()
