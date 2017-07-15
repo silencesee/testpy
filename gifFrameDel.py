59e47007bbc18670bfed74ff0a8f832d6189b628
sinlencesee <858713765@qq.com>
2017-07-15 13:40:13 +0800

    Default

--- gifFrameDel.py
+++ gifFrameDel.py
@@ -7,7 +7,25 @@ import os
 import sys
 
 class gi(object):
-    def __init__(self,):
+    def __init__(self,inputPath,outputPath):
+        self.inputPath = inputPath
+        self.outputPath = outputPath
+    def getBigGIF(self):
+        abspath = os.path.abspath(os.path.dirname(sys.argv[0]))
+        fullFname = []
+        for parent, dirnames, filenames in os.walk(abspath + self.inputPath):  # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
+            # for dirname in dirnames:  # 输出文件夹信息
+            #     print("父目录: ".decode('gbk').encode('utf-8') + parent.decode('gbk').encode('utf-8'))
+            #     print("子目录 ".decode('gbk').encode('utf-8') + dirname.decode('gbk').encode('utf-8'))
+
+            for filename in filenames:  # 输出文件信息
+
+                if filename[-3:] == 'gif':
+                    fullFname +=[parent + '\\' + filename]
+                    fileFormat = os.stat(fullFname[-1])  #  获取文件格式
+                    fileSize = fileFormat.st_size / 1024 / 1024  #  计算并判断文件大小是否超过2M
+                    if fileSize > 2:
+                        randomDel(parent, filename)
 
 def randomDel(parent,filename):
     gifpath = parent + '\\' + filename;
